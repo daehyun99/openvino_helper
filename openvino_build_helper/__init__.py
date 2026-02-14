7a30a80a783915cca51fc9cbf8150efea736fe17
김대현 (Kim DaeHyun) <daehyun99@kakao.com>
2026-02-15 02:36:28 +0900

    2

--- openvino_build_helper/__init__.py
+++
@@ -1,3 +0,0 @@
-from openvino_build_helper.save import *
-from openvino_build_helper.load import *
-from openvino_build_helper.setting import *
---
+++ openvino_helper/__init__.py
@@ -0,0 +1,3 @@
+from openvino_helper.save import *
+from openvino_helper.load import *
+from openvino_helper.setting import *
