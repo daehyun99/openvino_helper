544f858ec0f3976fc284d2a9582031d125920d71
김대현 (Kim DaeHyun) <daehyun99@kakao.com>
2026-02-15 02:03:20 +0900

    Delete BASE_PATH

--- openvino_build_helper/setting.py
+++ openvino_build_helper/setting.py
@@ -1,20 +1,10 @@
 
 def setting(
-    DRIVE_CACHE_ARCHIVE: str,
-    DRIVE_BIN_ARCHIVE: str,
+    DRIVE_CACHE_ARCHIVE: str = "/content/drive/MyDrive/Build_Cache/ccache_openvino_TF_Front.tar.gz",
+    DRIVE_BIN_ARCHIVE: str = "/content/drive/MyDrive/Build_Cache/ccache_openvino_TF_Front_bin.tar.gz",
     LOCAL_CACHE_DIR: str = "/content/ccache_local",
-    LOCAL_BIN_DIR: str = "content/openvino/bin"
+    LOCAL_BIN_DIR: str = "/content/openvino/bin"
     ):
-    BASE_PATH = "/content/drive/MyDrive/Build_Cache/"
-    # ==========================================
-    # 1. 경로 설정 (CCACHE + BIN)
-    # ==========================================
-    # ccache 경로
-    DRIVE_CACHE_ARCHIVE = BASE_PATH + "ccache_openvino_build_TF_Front.tar.gz"
-    DRIVE_BIN_ARCHIVE = BASE_PATH + "ccache_openvino_build_TF_Front_bin.tar.gz"
-    LOCAL_CACHE_DIR = LOCAL_CACHE_DIR
-    LOCAL_BIN_DIR = LOCAL_BIN_DIR
-
     SETTING = {}
     SETTING["DRIVE_CACHE_ARCHIVE"] = DRIVE_CACHE_ARCHIVE
     SETTING["DRIVE_BIN_ARCHIVE"] = DRIVE_BIN_ARCHIVE
